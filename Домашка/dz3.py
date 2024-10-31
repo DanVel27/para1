9b35d8ec508a3d3e7b532e2638de73540ab9987b
DanVel27 <velinovdaniil@gmail.com>
2024-10-31 12:56:49 +0200

    sss

--- Домашка/dz3.py
+++ Домашка/dz3.py
@@ -1,12 +1,12 @@
 class Product:
     def __init__(self):
-        self.name = "Bread"
-        self.price = 30
-        self.quatity = 5
+        self.name = "bread"
+        self.price = 5
+        self.quantity = 50
+        self.calculate_total_price = self.price * self.quantity
 
-    def printer(self):
-        print(f"{self.name}{self.price}{self.quatity}")
-
+    def display_info(self):
+        print(f"{self.name} cost {self.price} dollar, and all cost {self.calculate_total_price}.")
 
 product1 = Product()
-product1.printer()
+product1.display_info()
