a19f23ff21757f69012fe2942135777abad6190f
DanVel27 <velinovdaniil@gmail.com>
2024-11-22 21:09:02 +0200

    hello

--- Домашка/dz8.py
+++ Домашка/dz8.py
@@ -13,13 +13,47 @@ class Player(Character):
     def introduce(self, playr):
         print("Player's name is", playr.name)
 
+    def special_move(self):
+        print(f"{self.name} used special move!")
+
+    def health_manager(self, health):
+        self.health = health
+
+        def manage_health(amount):
+            if amount == 0:
+                health += amount
+            return health
+
+        return manage_health
+
 
 class Inventory:
     def __init__(self):
         self.items = []
 
-    def add_item(item):
-        print("Added", item.name, "to inventory")
+    def add_item(self):
+        print("Added", self.name, "to inventory")
 
+    def __iter__(self):
+        for item in player.inventory:
+            item = self.items
+
+    def special_ability(func):
+        def wrapper(self):
+            if getattr(self, "_ability_uses", 3) > 0:
+                self._ability_uses -= 1
+                return func(self)
+            else:
+                print("Special ability has no uses left!")
+
+        return wrapper
+
+
+def find_item(inventory, item_name):
+    for item in inventory:
+        if item == item_name:
+            yield item
+    yield "Item not found"
 
 character1 = Character("Grinch")
+player = Player("Hero")
---
+++ Классная работа/yrok5.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+from Домашка.dz8 import Player
+
+
+class GameCharacter(ABC):
+    @abstractmethod
+    def attack(self):
+        pass
+
+
+class Warrior(GameCharacter):
+    def attack(self):
+        return "Sword"
+
+
+class Mage(GameCharacter):
+    def attack(self):
+        return "Magic"
+
+
+warrior1 = Warrior()
+mage1 = Mage()
+print(warrior1.attack())
+print(mage1.attack())
+
+
+class PlayerInventory:
+    def __init__(self, items):
+        self.__items = items
+
+    def add_item(self, item):
+        if item > 0:
+            self.__items += item
+
+    def remove_item(self, item):
+        if item > 0:
+            self.__items -= item
+        else:
+            print("No items in inventory")
+
+    def show_inventory(self):
+        print(self.__items)
+
+
+player1 = PlayerInventory(1)
+player1.show_inventory()
+player1.add_item(4)
+player1.show_inventory()
+player1.remove_item(2)
+player1.show_inventory()
